76560994208a55b37f4ecd0dcf8a12ea9db26f4f
kevinbernat05 <kevromber@alu.edu.gva.es>
2026-01-12 07:47:16 +0000

    adasdasddsa

--- migrations/Version20260112073026.php
+++ migrations/Version20260112073026.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260111120042 extends AbstractMigration
+final class Version20260112073026 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20260111120042 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE palabra ADD palabra VARCHAR(255) NOT NULL, CHANGE texto definicion LONGTEXT NOT NULL');
         $this->addSql('ALTER TABLE usuario ADD foto_perfil VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE palabra DROP palabra, CHANGE definicion texto LONGTEXT NOT NULL');
         $this->addSql('ALTER TABLE usuario DROP foto_perfil');
     }
 }
