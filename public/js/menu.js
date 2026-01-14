a91719cc902e3740cae6909dabcb43e4595bf24a
AnaGabrielaSoroiu <anasor2@alu.edu.gva.es>
2026-01-14 08:37:43 +0000

    14/01/2026

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,6 +1,10 @@
 const menuBar = document.querySelector('.menu-bar-left');
 const toggleIcon = document.getElementById('toggle-menu');
 const toggleArrow = document.getElementById('toggle-arrow');
+const btnPublish = document.querySelector(".publicar");
+const overlay = document.getElementById("blur");
+const windoww = document.getElementById("window");
+const closeWindow = document.getElementById("close-window");
 
 // Click en icono de menú para esconder barra
 toggleIcon.addEventListener('click', function() {
@@ -14,4 +18,20 @@ toggleArrow.addEventListener('click', function () {
     menuBar.style.transform = 'translateX(0)';
     menuBar.style.opacity = '1';
     toggleArrow.style.opacity = '0'; // desaparece suavemente
+});
+
+// Menú de publicar al darle click al boton
+btnPublish.addEventListener("click", function(){
+    windoww.style.display = "block";
+    overlay.style.display = "block";
+});
+
+closeWindow.addEventListener("click", function(){
+    windoww.style.display = "none";
+    overlay.style.display = "none";
+});
+
+overlay.addEventListener("click", function(){
+    windoww.style.display = "none";
+    overlay.style.display = "none"
 });
